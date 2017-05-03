5c5da71b2178c7c8c1824b9911504cb0a4e90684
Alexpham688 <Alexpham688@gmail.com>
2017-05-02 21:06:30 -0400

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var app =angular.module('ourModule', []); 
+var app =angular.module('ourModule', []);
 
 app.controller('ourController', function($scope) {
 	//array pull words form
 	$scope.wordBank = [
 		{text: 'Alphabet'},
 		{text: 'Baby'},
+		{text: 'Bike'},
+		{text: 'Skate'},
+		{text: 'Big Wheels'},
+		{text: 'Momma'},
+		{text: 'Fish'},
 		{text: 'Apple'},
 		{text: 'Bed'},
 		{text: 'Book'},
@@ -17,12 +22,31 @@ app.controller('ourController', function($scope) {
 		{text: 'Ice-cream'},
 		{text: 'Kaleidoscope'},
 		{text: 'Rainbow'},
-		{text: 'Sports-car'},
-		{text: 'Television'},
-		{text: 'Sucka'},
-		{text: 'Terrible'},
+
 	];
-	
+	$scope.wordsBanks = [
+		{text: 'Alphabet',cls:"nice"},
+		{text: 'Baby',cls:"nice"},
+		{text: 'Bike', cls: "nice"},
+		{text: 'Skate', cls:"nice"},
+		{text: 'Big Wheels',cls:"nice"},
+		{text: 'Momma', cls:"nice"},
+		{text: 'Fish', cls:"nice"},
+		{text: 'Apple', cls:"nice"},
+		{text: 'Bed', cls:"nice"},
+		{text: 'Book', cls:"nice"},
+		{text: 'Butterfly', cls:"nice"},
+		{text: 'Chocolates', cls:"nice"},
+		{text: 'Circus', cls:"nice"},
+		{text: 'Computer', cls:"nice"},
+		{text: 'Drum', cls:"nice"},
+		{text: 'Flower', cls:"nice"},
+		{text: 'Ice-cream', cls:"nice"},
+		{text: 'Kaleidoscope', cls:"nice"},
+		{text: 'Rainbow', cls:"nice"},
+
+	];
+
 	$scope.fancyWordBank = [
 		{text: 'Art' ,cls:"fancy1"},
 		{text: 'Dance' ,cls:"fancy1"},
@@ -34,9 +58,29 @@ app.controller('ourController', function($scope) {
 		{text: 'Carnival' ,cls:"fancy1"},
 		{text: 'Toy Store' ,cls:"fancy1"},
 		{text: 'Science' ,cls:"fancy1"},
+		{text: 'Sports-car',cls:"fancy1"},
+		{text: 'Television',cls:"fancy1"},
+		{text: 'Sucka',cls:"fancy1"},
+		{text: 'Terrible', cls:"fancy1"},
 	];
-	
-	
+	$scope.fancysWordBank = [
+		{text: 'Art' ,cls:"fancy2"},
+		{text: 'Dance' ,cls:"fancy2"},
+		{text: 'Dogs' ,cls:"fancy2"},
+		{text: 'Fish' ,cls:"fancy2"},
+		{text: 'Fruit' ,cls:"fancy2"},
+		{text: 'People' ,cls:"fancy2"},
+		{text: 'Farm' ,cls:"fancy2"},
+		{text: 'Carnival', cls:"fancy2"},
+		{text: 'Toy Store' ,cls:"fancy2"},
+		{text: 'Science' ,cls:"fancy2"},
+		{text: 'Sports-car',cls:"fancy2"},
+		{text: 'Television',cls:"fancy2"},
+		{text: 'Sucka',cls:"fancy2"},
+		{text: 'Terrible', cls:"fancy2"},
+	];
+
+
 	$scope.displayWords = [];
 
 	$scope.addWord = function(){
@@ -46,21 +90,21 @@ app.controller('ourController', function($scope) {
 	};
 
 	$scope.addWords = function(){
-		$scope.randomWord = $scope.wordBank[Math.floor(Math.random() * $scope.wordBank.length)]
+		$scope.randomWord = $scope.wordsBanks[Math.floor(Math.random() * $scope.wordsBanks.length)]
 		$scope.displayWords.push($scope.randomWord);
-		$scope.randomWord = $scope.wordBank[Math.floor(Math.random() * $scope.wordBank.length)]
+		$scope.randomWord = $scope.wordsBanks[Math.floor(Math.random() * $scope.wordsBanks.length)]
 		$scope.displayWords.push($scope.randomWord);
-		$scope.randomWord = $scope.wordBank[Math.floor(Math.random() * $scope.wordBank.length)]
-		$scope.displayWords.push($scope.randomWord);
-		console.log($scope.displayWords);
-	};
-	
-	$scope.addFancyWord = function(){
-		$scope.randomWord = $scope.fancyWordBank[Math.floor(Math.random() * $scope.fancyWordBank.length)]
+		$scope.randomWord = $scope.wordsBanks[Math.floor(Math.random() * $scope.wordsBanks.length)]
 		$scope.displayWords.push($scope.randomWord);
 		console.log($scope.displayWords);
 	};
-	
+
+	$scope.addsFancyWord = function(){
+		$scope.randomWord = $scope.fancysWordBank[Math.floor(Math.random() * $scope.fancysWordBank.length)]
+		$scope.displayWords.push($scope.randomWord);
+		console.log($scope.displayWords);
+	};
+
 	$scope.addFancyWords = function(){
 		$scope.randomWord = $scope.fancyWordBank[Math.floor(Math.random() * $scope.fancyWordBank.length)]
 		$scope.displayWords.push($scope.randomWord);
